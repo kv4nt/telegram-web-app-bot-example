c52b8eccaa881c93692d3f665b7a03efb09a61fd
shumeikov <viktor.shumeiko@simbirsoft.com>
2025-01-22 15:20:09 +0300

    test

--- game/assets/js/init.js
+++ game/assets/js/init.js
@@ -717,7 +717,7 @@ function buildGameCanvas() {
     statBallsLabelTxt.x = 20;
     statBallsLabelTxt.y = -5;
     statBallsTxt = new createjs.Text;
-    statBallsTxt.font = "30px Stalinist One";
+    statBallsTxt.font = "10px Stalinist One";
     statBallsTxt.color = "#fff";
     statBallsTxt.textAlign = "left";
     statBallsTxt.textBaseline = "alphabetic";
@@ -735,7 +735,7 @@ function buildGameCanvas() {
     statsBallsContainer.addChild(itemStatBalls, statBallsLabelTxt, statBallsTxt, itemBallsDragBar, itemBallsDrag);
     buttonBlank = new createjs.Bitmap(loader.getResult("buttonBlank"));
     buttonBlankTxt = new createjs.Text;
-    buttonBlankTxt.font = "2px Stalinist One";
+    buttonBlankTxt.font = "20px Stalinist One";
     buttonBlankTxt.color = "#fff";
     buttonBlankTxt.textAlign = "center";
     buttonBlankTxt.textBaseline = "alphabetic";
