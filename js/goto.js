377fc64d6dae1be5924242bfbb0d357fd476ff8f
shumeikov <viktor.shumeiko@simbirsoft.com>
2025-01-28 15:31:39 +0300

    test

---
+++ js/goto.js
@@ -0,0 +1,33 @@
+function gotoBank() {
+    window.location.href = 'bank.html';
+}
+
+function goToDemo() {
+    window.location.href = 'demo.html';
+}
+
+function gotoMatch() {
+    window.location.href = 'match3.html';
+}
+
+function gotoMatchDemo() {
+    window.location.href = 'match.html';
+}
+function gotoTowerDemo() {
+    window.location.href = 'tower.html';
+}
+
+function gotoCasino() {
+    window.location = window.location.origin+'/telegram-web-app-bot-example/game/index.html';
+}
+
+function gotoGames() {
+    window.location.href = 'games.html';
+}
+
+$(document).ready(function () {
+    setTimeout(function () {
+        $('.loader').addClass('d-none');
+    },12500)
+
+})
